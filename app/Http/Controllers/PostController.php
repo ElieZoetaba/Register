46fc6c2ec7688b727087a18e3dc59aee8a36a699
Elie <99994051+ElieZoetaba@users.noreply.github.com>
2022-05-30 22:44:28 +0000

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = post::all();
+        return view('crud.tasks')->with('tasks', $students);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      return view('crud.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        post::create($input);
+        return redirect('create')->with('flash_message', 'post Added');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $students= post::find($id);
+        return view('crud.show')->with('show', $students);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $students= post::find($id);
+        return view('crud.edit')->with('edit', $students);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $students =post::find($id);
+        $input = $request->all();
+        $students->update($input);
+        return redirect('create')->with('flash_massage', 'post Updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        post::destroy($id);
+        return redirect('create')->with('flash_message', 'post deleted!');
+    }
+}
